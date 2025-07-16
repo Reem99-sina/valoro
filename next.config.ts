673c73eb1bb17f0e80792b90e722bb9e8fc79b64
unknown <reemsina99@gmail.com>
2025-07-16 10:46:16 +0300

    laning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 const withSvgr = require("next-plugin-svgr");
+import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     // remotePatterns: [
     //   {
@@ -12,6 +14,17 @@ const nextConfig = {
     //   },
     // ],
     domains: ["rational-unity-e3171a749a.media.strapiapp.com", "localhost"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      type: "asset/resource",
+      generator: {
+        filename: "static/images/[name].[hash][ext]",
+      },
+    });
+
+    return config;
   },
 };
 
